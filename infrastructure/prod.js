100e911e1dd286279c08644466a64b7fd0d07a9b
Patryk Konior <patryk.konior@gmail.com>
2016-01-07 09:50:49 +0100

    changed mongodb url

--- infrastructure/prod.js
+++ infrastructure/prod.js
@@ -3,7 +3,7 @@ var _ = require('lodash');
 
 var prod = {
     name: 'node-microservices-ws',
-    config_vars: { MONGOLAB_URI: 'mongodb://heroku_4x78mgjj:5c76vmaot6lpjfdubl2k6f1qc4@ds033285.mongolab.com:33285/heroku_4x78mgjj' },
+    config_vars: { MONGOLAB_URI: 'mongodb://heroku_rc6jxkln:jl60qp4df6khbm6tnh4j9139g6@ds035975.mongolab.com:35975/heroku_rc6jxkln' },
     domains: [ 'node-microservices-ws.herokuapp.com' ]
 };
 
--- infrastructure/test.js
+++ infrastructure/test.js
@@ -4,8 +4,8 @@ var _ = require('lodash');
 var test = {
     name: 'node-microservices-ws-test',
     config_vars: {
-        MONGOLAB_URI: 'mongodb://heroku_4x78mgjj:5c76vmaot6lpjfdubl2k6f1qc4@ds033285.mongolab.com:33285/heroku_4x78mgjj',
-        NODE_ENV: 'test' 
+        MONGOLAB_URI: 'mongodb://heroku_rc6jxkln:jl60qp4df6khbm6tnh4j9139g6@ds035975.mongolab.com:35975/heroku_rc6jxkln',
+        NODE_ENV: 'test'
     },
     domains: [ 'node-microservices-ws-test.herokuapp.com' ]
 };
